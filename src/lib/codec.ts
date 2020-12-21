6df2399f6bd92f30306ecfb718c41c8adfc524c3
pfcoder <pfcoder97@gmail.com>
2020-12-21 20:58:45 +0800

    update types

--- src/lib/codec.ts
+++ src/lib/codec.ts
@@ -1,20 +1,42 @@
-import { Struct } from '@polkadot/types';
+import { Bytes, GenericAccountId, Struct, U8aFixed } from '@polkadot/types';
 import { TypeRegistry } from '@polkadot/types/create';
-import { Text, u32 } from '@polkadot/types/primitive';
-
-//import { AppFinancedProposalParams } from './types';
+import { u128, u32, u64, u8 } from '@polkadot/types/primitive';
 
 const registry = new TypeRegistry();
 
-function processAppFinancedProposalParams(_params: any): Uint8Array {
-  /*const s: AppFinancedProposalParams = params as AppFinancedProposalParams;
-  console.log("s:", s);
-  return s.toU8a();*/
-
+function processAppFinancedProposalParams(params: any): Uint8Array {
   const s = new Struct(registry, {
-    foo: Text,
-    bar: u32,
-  }, { foo: 'bazzing', bar: 10 });
+    account: GenericAccountId,
+    appId: u32,
+    proposalId: Bytes,
+    exchange: u128,
+    amount: u128,
+  }, params);
+
+  return s.toU8a();
+}
+
+function processCommentDataParams(params: any): Uint8Array {
+  const s = new Struct(registry, {
+    appId: u32,
+    documentId: Bytes,
+    commentId: Bytes,
+    commentHash: U8aFixed,
+    commentFee: u64,
+    commentTrend: u8,
+  }, params);
+
+  return s.toU8a();
+}
+
+function processAddAppParams(params: any): Uint8Array {
+  const s = new Struct(registry, {
+    appType: Bytes,
+    appName: Bytes,
+    appKey: GenericAccountId,
+    appAdminKey: GenericAccountId,
+    returnRate: u32,
+  }, params);
 
   return s.toU8a();
 }
@@ -23,6 +45,10 @@ export const encode = (structType: string, structObj: any): Uint8Array => {
   switch (structType) {
     case 'AppFinancedProposalParams':
       return processAppFinancedProposalParams(structObj);
+    case 'CommentData':
+      return processCommentDataParams(structObj);
+    case 'AddAppParams':
+      return processAddAppParams(structObj);
     default:
       return null;
       break;
